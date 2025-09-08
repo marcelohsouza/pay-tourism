02e7de13918294f6c4d63ecb894e07046a4a9a22
marcelohsouza <marcelo.souza1@edu.fecap.br>
2025-09-08 16:21:58 -0300

    feat(auth): add register and login routes with JWT implementation

---
+++ backend/src/lib/auth.ts
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+import  jwt  from "jsonwebtoken";
+
+const JWT_SRECET = process.env.JWT_SECRET || "supersecretkey";
+
+export const hashPassword = async (password: string) => {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+};
+
+export const comparePassword = async (password: string, hash: string) => {
+    return bcrypt.compare(password, hash);
+};
+
+export const generateToken = (userId: number, role: string) => {
+    return jwt.sign({ userId, role}, JWT_SRECET, { expiresIn: "1d"});
+};
+
+export const verifyToken = (token: string) => {
+    return jwt.verify(token, JWT_SRECET);
+}
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import prisma from "../lib/prisma";
+import { hashPassword, comparePassword, generateToken } from "../lib/auth";
+
+
+const router = Router();
+
+// Registrando usuario
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+        if (existingUser) return res.status(400).json({ error: "Email já cadastrado"});
+
+        const hashedPassword = await hashPassword(password);
+        const user = await prisma.user.create({
+            data: { name, email, password: hashedPassword },
+        });
+
+        const token = generateToken(user.id, user.role);
+        res.json({ user: { id: user.id, name: user.name, email: user.email }, token });
+    } catch (err) {
+        res.status(500).json({ error: "Erro ao registrar usuário" });
+    }
+})
+
+// Login
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {  
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) return res.status(400).json({ error: "Usuário não encontrado" });
+        
+        const isValid = await comparePassword(password, user.password);
+        if (!isValid) return res.status(400).json({ error: "Senha incorreta" });
+
+        const token = generateToken(user.id, user.role);
+        res.json({ user: { id: user.id, name: user.name, email: user.email }, token });
+    } catch (err) {
+        res.status(500).json({ error: "Erro ao fazer login" });
+    }
+});
+
+export default router;
